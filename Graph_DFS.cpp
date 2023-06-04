5d6acfb9a7c0862947dbda9f57055608959008a2
Rishabh Singhal <123990866+RishabhSinghal98@users.noreply.github.com>
2023-06-04 21:48:54 +0530

    Add files via upload

---
+++ Graph_DFS.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void dfs(int source,vector<int>&visited,vector<vector<int>>&graph)
+{
+	visited[source]=1; //marking the source in visited array to 1 i.e visited
+	
+	cout<<source<<"->"; //printing the nodes
+	
+	for(auto x:graph[source]) // traversing in the list/vecctor coonected with the vector i.e child 
+	{
+		if(visited[x]==0) //checking whether the child is visited or not
+		{
+			dfs(x,visited,graph); // recuursively calling dfs again to go to the last node through the child nodes 
+		}
+	}
+	
+}
+
+void creategraph(int node1,int node2,vector<vector<int>>& graph)
+{
+    // creating graph pushing node2 at vector node1 index
+	graph[node1].push_back(node2);
+	//creating graph pushing node1 at vector node2 index
+	graph[node2].push_back(node1);
+}
+int main()
+{
+	
+	int vertices=5;
+
+	vector<vector<int>> graph(vertices); //created graph of size=no of vertices
+	
+	creategraph(0,1,graph);
+	creategraph(0,2,graph);
+	creategraph(1,2,graph);
+	creategraph(2,0,graph);
+	creategraph(2,3,graph);
+	creategraph(3,3,graph);
+	
+	// creating an array to mark visited nodes initally witl all zeroes
+	vector<int> visited(vertices,0);
+	
+	dfs(2,visited,graph); // 2 is the source node
+	
+	
+	return 0;
+	
+}
